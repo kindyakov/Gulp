3228540a625e504c66d32c198ce8a6448bc9b0bc
kindyakov <kindyakov_2016@mail.ru>
2023-08-10 13:28:22 +0300

    Исправление ошибок

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -45,7 +45,7 @@ export const ttfToWoff = () => {
 
 export const fontsStyle = () => {
   //Файл стилей подключения шрифтов
-  let fontsFile = `${app.path.srcFolder}/scss/fonts.scss`;
+  let fontsFile = `${app.path.srcFolder}/scss/settings/fonts.scss`;
   //Проверяем, существуют ли файлы шрифтов
   fs.readdir(app.path.build.fonts, function (err, fontsFiles) {
     if (fontsFiles) {
@@ -85,7 +85,7 @@ export const fontsStyle = () => {
         }
       } else {
         //Если файл есть, выводим сообщение
-        console.log("Файл scss/fonts.scss уже существует. Для обновления файла нужно его удалить!");
+        console.log("Файл scss/settings/fonts.scss уже существует. Для обновления файла нужно его удалить!");
       }
     }
   });
