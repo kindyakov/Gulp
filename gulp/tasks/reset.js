b078ab48bb7d38cbbe149e19e3f536876f0d705c
kindyakov <kindyakov_2016@mail.ru>
2023-08-11 14:47:33 +0300

    Добавил файлы

--- gulp/tasks/reset.js
+++ gulp/tasks/reset.js
@@ -1,7 +1,7 @@
 import { deleteAsync } from "del"
 
 const reset = () => {
-  return deleteAsync(app.path.clean)
+  return deleteAsync([`${app.path.clean}/**/*`, `!${app.path.clean}/.git`, `!${app.path.clean}/.gitignore`])
 }
 
 export default reset
